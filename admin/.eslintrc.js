d5ed084ffb99ca8aa9875591251e4bd711c6a1ed
Dinhhuy0810 <102515280+HuyXinLoi@users.noreply.github.com>
2025-04-07 22:04:54 +0700

    Update .eslintrc.js

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
     extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-img-element': 'off',
-    },
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      '@next/next/no-img-element': 'warn',
+    }
   }
   
